3cf863dda379f3a98cdcb45f532b14804671ff54
芒果集团 <1282261308@qq.com>
2024-05-27 18:04:07 +0800

    0527

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -671,47 +671,79 @@ import os
 # 过道的外侧修一圈围墙 已知过道的造价为25元每平米 围墙的造价为32.5元每米
 # 输出围墙和过道的总造价分别是多少钱(精确到小数点后2位)
 
-import math
-
-class Circle(object):
-
-    def __init__(self,radius):
-        self.radius = radius
-
-    @property
-    def radius(self):
-        return self._radius
-
-    @radius.setter
-    def radius(self,radius):
-        self._radius = radius if radius > 0 else 0
-
-    @property
-    def perimeter(self):
-        return 2 * math.pi * self._radius
-
-    @property
-    def area(self):
-        return math.pi * self._radius * self._radius
-
-if __name__ == '__main__':
-    radius = float(input("Enter radius: "))
-    small = Circle(radius)
-    big = Circle(radius+3)
-    print('围墙的造假为：￥$.1f元' % (big.perimeter * 115))
-    print('过道的造假为：￥%.1f元' % ((big.area - small.area) * 65))
-
-
+# import math
+#
+# class Circle(object):
+#
+#     def __init__(self,radius):
+#         self._radius = radius
+#
+#     @property
+#     def radius(self):
+#         return self._radius
+#
+#     @radius.setter
+#     def radius(self,radius):
+#         self._radius = radius if radius > 0 else 0
+#
+#     @property
+#     def perimeter(self):
+#         return 2 * math.pi * self._radius
+#
+#     @property
+#     def area(self):
+#         return math.pi * self._radius * self._radius
+#
+# if __name__ == '__main__':
+#     radius = float(input("Enter radius: "))
+#     small = Circle(radius)
+#     big = Circle(radius+3)
+#     print('围墙的造假为：￥$.1f元' % (big.perimeter * 115))
+#     print('过道的造假为：￥%.1f元' % ((big.area - small.area) * 65))
 
 
+# 定义和使用时钟类
 
+# import time
+# import os
+#
+# class Clock(object):
+#     # Python中的函数是没有重载的概念的
+#     # 因为Python中函数的参数没有类型而且支持缺省参数和可变参数
+#     # 用关键字参数让构造器可以传入任意多个参数来实现其他语言中的构造器重载
+#     def __init__(self,**kw):
+#         if 'houer' in kw and 'minute' in kw and 'second' in kw:
+#             self._houer = kw['houer']
+#             self._minute = kw['minute']
+#             self._second = kw['second']
+#         else:
+#             tm = time.localtime(time.time())
+#             self._houer =tm.tm_hour
+#             self._minute = tm.tm_min
+#             self._second = tm.tm_sec
+#
+#     def run(self):
+#         self._second +=1
+#         if self._second == 60:
+#             self._second = 0
+#             self._minute += 1
+#             if self._minute == 60:
+#                 self._minute = 0
+#                 self._houer += 1
+#                 if self._houer == 24:
+#                     self._houer = 0
+#
+#     def show(self):
+#         return '%02d:%02d:%02d' % (self._houer, self._minute, self._second)
+#
+# clock = Clock()
+# while True:
+#     os.system('cls')
+#     print(clock.show())
+#     time.sleep(1)
+#     clock.run()
 
-
-
-
-
-
-
+# 面向对象版本的猜数字游戏
 
 
 
