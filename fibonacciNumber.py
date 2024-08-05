8f74456f40e1877ae69687dad1aff5856ece79f3
芒果集团 <1282261308@qq.com>
2024-08-05 18:02:02 +0800

    大球吃小球

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -1277,73 +1277,182 @@ import os
 #     main()
 
 # 运算符重载 - 自定义分数类
+#
+# from math import gcd
+#
+# class Rational(object):
+#     def __init__(self, num, den=1):
+#         if den == 0:
+#             raise ValueError('分母不能为0')
+#         self._num = num
+#         self._den = den
+#         self.normalize()
+#
+#     def simplify(self):
+#         x = abs(self._num)
+#         y = abs(self._den)
+#         factor = gcd(x, y)
+#         if factor > 1:
+#             self._den //= factor
+#             self._num //= factor
+#         return self
+#
+#     def normalize(self):
+#         if self._den < 0:
+#             self._den = -self._den
+#             self._num = -self._num
+#         return self
+#
+#     def __add__(self, other):
+#         new_num = self._num * other._den + other._num * self._den
+#         new_den = self._den * other._den
+#         return Rational(new_num, new_den).simplify().normalize()
+#
+#     def __sub__(self, other):
+#         new_num = self._num * other._den - other._num * self._den
+#         new_den = self._den * other._den
+#         return Rational(new_num, new_den).simplify().normalize()
+#
+#     def __mul__(self, other):
+#         new_num = self._num * other._num
+#         new_den = self._den * other._den
+#         return Rational(new_num, new_den).simplify().normalize()
+#
+#     def __truediv__(self, other):
+#         new_num = self._num * other._den
+#         new_den = self._den * other._num
+#         return Rational(new_num, new_den).simplify().normalize()
+#
+#     def __str__(self):
+#         if self._num == 0:
+#             return '0'
+#         elif self._den == 1:
+#             return str(self._num)
+#         else:
+#             return '(%d/%d)' % (self._num, self._den)
+#
+# if __name__ == '__main__':
+#     r1 = Rational(2, 3)
+#     print(r1)
+#     r2 = Rational(6, -8)
+#     print(r2)
+#     print(r2.simplify())
+#     print('%s + %s = %s' % (r1, r2, r1 + r2))
+#     print('%s - %s = %s' % (r1, r2, r1 - r2))
+#     print('%s * %s = %s' % (r1, r2, r1 * r2))
+#     print('%s / %s = %s' % (r1, r2, r1 / r2))
 
-from math import gcd
+# 游戏
 
-class Rational(object):
-    def __init__(self, num, den=1):
-        if den == 0:
-            raise ValueError('分母不能为0')
-        self._num = num
-        self._den = den
-        self.normalize()
+from enum import Enum, unique
+from math import sqrt
+from random import randint
 
-    def simplify(self):
-        x = abs(self._num)
-        y = abs(self._den)
-        factor = gcd(x, y)
-        if factor > 1:
-            self._den //= factor
-            self._num //= factor
-        return self
+import pygame
 
-    def normalize(self):
-        if self._den < 0:
-            self._den = -self._den
-            self._num = -self._num
-        return self
 
-    def __add__(self, other):
-        new_num = self._num * other._den + other._num * self._den
-        new_den = self._den * other._den
-        return Rational(new_num, new_den).simplify().normalize()
+@unique
+class Color(Enum):
+    """颜色"""
 
-    def __sub__(self, other):
-        new_num = self._num * other._den - other._num * self._den
-        new_den = self._den * other._den
-        return Rational(new_num, new_den).simplify().normalize()
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GRAY = (242, 242, 242)
 
-    def __mul__(self, other):
-        new_num = self._num * other._num
-        new_den = self._den * other._den
-        return Rational(new_num, new_den).simplify().normalize()
+    @staticmethod
+    def random_color():
+        """获得随机颜色"""
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        return (r, g, b)
 
-    def __truediv__(self, other):
-        new_num = self._num * other._den
-        new_den = self._den * other._num
-        return Rational(new_num, new_den).simplify().normalize()
 
-    def __str__(self):
-        if self._num == 0:
-            return '0'
-        elif self._den == 1:
-            return str(self._num)
-        else:
-            return '(%d/%d)' % (self._num, self._den)
+class Ball(object):
+    """球"""
+
+    def __init__(self, x, y, radius, sx, sy, color=Color.RED):
+        """初始化方法"""
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.sx = sx
+        self.sy = sy
+        self.color = color
+        self.alive = True
+
+    def move(self, screen):
+        """移动"""
+        self.x += self.sx
+        self.y += self.sy
+        if self.x - self.radius <= 0 or self.x + self.radius >= screen.get_width():
+            self.sx = -self.sx
+        if self.y - self.radius <= 0 or self.y + self.radius >= screen.get_height():
+            self.sy = -self.sy
+
+    def eat(self, other):
+        """吃其他球"""
+        if self.alive and other.alive and self != other:
+            dx, dy = self.x - other.x, self.y - other.y
+            distance = sqrt(dx ** 2 + dy ** 2)
+            if distance < self.radius + other.radius \
+                    and self.radius > other.radius:
+                other.alive = False
+               	self.radius = self.radius + int(other.radius * 0.146)
+
+    def draw(self, screen):
+        """在窗口上绘制球"""
+        pygame.draw.circle(screen, self.color,
+                           (self.x, self.y), self.radius, 0)
+
+
+def main():
+    # 定义用来装所有球的容器
+    balls = []
+    # 初始化导入的pygame中的模块
+    pygame.init()
+    # 初始化用于显示的窗口并设置窗口尺寸
+    screen = pygame.display.set_mode((800, 600))
+    print(screen.get_width())
+    print(screen.get_height())
+    # 设置当前窗口的标题
+    pygame.display.set_caption('大球吃小球')
+    # 定义变量来表示小球在屏幕上的位置
+    x, y = 50, 50
+    running = True
+    # 开启一个事件循环处理发生的事件
+    while running:
+        # 从消息队列中获取事件并对事件进行处理
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = event.pos
+                radius = randint(10, 100)
+                sx, sy = randint(-10, 10), randint(-10, 10)
+                color = Color.random_color()
+                ball = Ball(x, y, radius, sx, sy, color)
+                balls.append(ball)
+        screen.fill((255, 255, 255))
+        for ball in balls:
+            if ball.alive:
+                ball.draw(screen)
+            else:
+                balls.remove(ball)
+        pygame.display.flip()
+        # 每隔50毫秒就改变小球的位置再刷新窗口
+        pygame.time.delay(50)
+        for ball in balls:
+            ball.move(screen)
+            for other in balls:
+                ball.eat(other)
+
 
 if __name__ == '__main__':
-    r1 = Rational(2, 3)
-    print(r1)
-    r2 = Rational(6, -8)
-    print(r2)
-    print(r2.simplify())
-    print('%s + %s = %s' % (r1, r2, r1 + r2))
-    print('%s - %s = %s' % (r1, r2, r1 - r2))
-    print('%s * %s = %s' % (r1, r2, r1 * r2))
-    print('%s / %s = %s' % (r1, r2, r1 / r2))
-
-
-
+    main()
 
 
 
