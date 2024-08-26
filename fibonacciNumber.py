8aa00fc20c9f73e28f0b9319286bda7726a90c45
芒果集团 <1282261308@qq.com>
2024-08-26 18:01:24 +0800

    '读写文件'

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -162,6 +162,9 @@ import math
 import time
 import shutil
 import os
+from fileinput import filename
+
+from pygame.draw import lines
 
 # seconds = time.time()
 # print('当前时间戳的秒数',seconds)
@@ -1344,115 +1347,309 @@ import os
 
 # 游戏
 
-from enum import Enum, unique
-from math import sqrt
-from random import randint
+# from enum import Enum, unique
+# from math import sqrt
+# from random import randint
+#
+# import pygame
+#
+#
+# @unique
+# class Color(Enum):
+#     """颜色"""
+#
+#     RED = (255, 0, 0)
+#     GREEN = (0, 255, 0)
+#     BLUE = (0, 0, 255)
+#     BLACK = (0, 0, 0)
+#     WHITE = (255, 255, 255)
+#     GRAY = (242, 242, 242)
+#
+#     @staticmethod
+#     def random_color():
+#         """获得随机颜色"""
+#         r = randint(0, 255)
+#         g = randint(0, 255)
+#         b = randint(0, 255)
+#         return (r, g, b)
+#
+#
+# class Ball(object):
+#     """球"""
+#
+#     def __init__(self, x, y, radius, sx, sy, color=Color.RED):
+#         """初始化方法"""
+#         self.x = x
+#         self.y = y
+#         self.radius = radius
+#         self.sx = sx
+#         self.sy = sy
+#         self.color = color
+#         self.alive = True
+#
+#     def move(self, screen):
+#         """移动"""
+#         self.x += self.sx
+#         self.y += self.sy
+#         if self.x - self.radius <= 0 or self.x + self.radius >= screen.get_width():
+#             self.sx = -self.sx
+#         if self.y - self.radius <= 0 or self.y + self.radius >= screen.get_height():
+#             self.sy = -self.sy
+#
+#     def eat(self, other):
+#         """吃其他球"""
+#         if self.alive and other.alive and self != other:
+#             dx, dy = self.x - other.x, self.y - other.y
+#             distance = sqrt(dx ** 2 + dy ** 2)
+#             if distance < self.radius + other.radius \
+#                     and self.radius > other.radius:
+#                 other.alive = False
+#                	self.radius = self.radius + int(other.radius * 0.146)
+#
+#     def draw(self, screen):
+#         """在窗口上绘制球"""
+#         pygame.draw.circle(screen, self.color,
+#                            (self.x, self.y), self.radius, 0)
+#
+#
+# def main():
+#     # 定义用来装所有球的容器
+#     balls = []
+#     # 初始化导入的pygame中的模块
+#     pygame.init()
+#     # 初始化用于显示的窗口并设置窗口尺寸
+#     screen = pygame.display.set_mode((800, 600))
+#     print(screen.get_width())
+#     print(screen.get_height())
+#     # 设置当前窗口的标题
+#     pygame.display.set_caption('大球吃小球')
+#     # 定义变量来表示小球在屏幕上的位置
+#     x, y = 50, 50
+#     running = True
+#     # 开启一个事件循环处理发生的事件
+#     while running:
+#         # 从消息队列中获取事件并对事件进行处理
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 running = False
+#             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+#                 x, y = event.pos
+#                 radius = randint(10, 100)
+#                 sx, sy = randint(-10, 10), randint(-10, 10)
+#                 color = Color.random_color()
+#                 ball = Ball(x, y, radius, sx, sy, color)
+#                 balls.append(ball)
+#         screen.fill((255, 255, 255))
+#         for ball in balls:
+#             if ball.alive:
+#                 ball.draw(screen)
+#             else:
+#                 balls.remove(ball)
+#         pygame.display.flip()
+#         # 每隔50毫秒就改变小球的位置再刷新窗口
+#         pygame.time.delay(50)
+#         for ball in balls:
+#             ball.move(screen)
+#             for other in balls:
+#                 ball.eat(other)
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-import pygame
+# 事件交互
+# import tkinter
+# import tkinter.messagebox
+#
+#
+# def main():
+#     flag = True
+#
+#     # 修改标签上的文字
+#     def change_label_text():
+#         nonlocal flag
+#         flag = not flag
+#         color, msg = ('red', 'Hello, world!')\
+#             if flag else ('blue', 'Goodbye, world!')
+#         label.config(text=msg, fg=color)
+#
+#     # 确认退出
+#     def confirm_to_quit():
+#         if tkinter.messagebox.askokcancel('温馨提示', '确定要退出吗?'):
+#             top.quit()
+#
+#     # 创建顶层窗口
+#     top = tkinter.Tk()
+#     # 设置窗口大小
+#     top.geometry('240x160')
+#     # 设置窗口标题
+#     top.title('小游戏')
+#     # 创建标签对象
+#     label = tkinter.Label(top, text='Hello, world!', font='Arial -32', fg='red')
+#     label.pack(expand=1)
+#     # 创建一个装按钮的容器
+#     panel = tkinter.Frame(top)
+#     # 创建按钮对象
+#     button1 = tkinter.Button(panel, text='修改', command=change_label_text)
+#     button1.pack(side='left')
+#     button2 = tkinter.Button(panel, text='退出', command=confirm_to_quit)
+#     button2.pack(side='right')
+#     panel.pack(side='bottom')
+#     # 开启主事件循环
+#     tkinter.mainloop()
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+# 画布视图
+# import tkinter
+#
+#
+# def mouse_evt_handler(evt=None):
+#     row = round((evt.y - 20) / 40)
+#     col = round((evt.x - 20) / 40)
+#     pos_x = 40 * col
+#     pos_y = 40 * row
+#     canvas.create_oval(pos_x, pos_y, 40 + pos_x, 40 + pos_y, fill='black')
+#
+#
+# top = tkinter.Tk()
+# # 设置窗口尺寸
+# top.geometry('620x620')
+# # 设置窗口标题
+# top.title('五子棋')
+# # 设置窗口大小不可改变
+# top.resizable(False, False)
+# # 设置窗口置顶
+# top.wm_attributes('-topmost', 1)
+# canvas = tkinter.Canvas(top, width=600, height=600, bd=0, highlightthickness=0)
+# canvas.bind('<Button-1>', mouse_evt_handler)
+# canvas.create_rectangle(0, 0, 600, 600, fill='yellow', outline='white')
+# for index in range(15):
+#     canvas.create_line(20, 20 + 40 * index, 580, 20 + 40 * index, fill='black')
+#     canvas.create_line(20 + 40 * index, 20, 20 + 40 * index, 580, fill='black')
+# canvas.create_rectangle(15, 15, 585, 585, outline='black', width=4)
+# canvas.pack()
+# tkinter.mainloop()
+
+# 窗口制作动画
 
 
-@unique
-class Color(Enum):
-    """颜色"""
+# import tkinter
+# import time
+#
+#
+# # 播放动画效果的函数
+# def play_animation():
+#     canvas.move(oval, 2, 2)
+#     canvas.update()
+#     top.after(50, play_animation)
+#
+#
+# x = 10
+# y = 10
+# top = tkinter.Tk()
+# top.geometry('600x600')
+# top.title('动画效果')
+# top.resizable(False, False)
+# top.wm_attributes('-topmost', 1)
+# canvas = tkinter.Canvas(top, width=600, height=600, bd=0, highlightthickness=0)
+# canvas.create_rectangle(0, 0, 600, 600, fill='gray')
+# oval = canvas.create_oval(10, 10, 60, 60, fill='red')
+# canvas.pack()
+# top.update()
+# play_animation()
+# tkinter.mainloop()
 
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    GRAY = (242, 242, 242)
+ # csv文件
 
-    @staticmethod
-    def random_color():
-        """获得随机颜色"""
-        r = randint(0, 255)
-        g = randint(0, 255)
-        b = randint(0, 255)
-        return (r, g, b)
-
-
-class Ball(object):
-    """球"""
-
-    def __init__(self, x, y, radius, sx, sy, color=Color.RED):
-        """初始化方法"""
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.sx = sx
-        self.sy = sy
-        self.color = color
-        self.alive = True
-
-    def move(self, screen):
-        """移动"""
-        self.x += self.sx
-        self.y += self.sy
-        if self.x - self.radius <= 0 or self.x + self.radius >= screen.get_width():
-            self.sx = -self.sx
-        if self.y - self.radius <= 0 or self.y + self.radius >= screen.get_height():
-            self.sy = -self.sy
-
-    def eat(self, other):
-        """吃其他球"""
-        if self.alive and other.alive and self != other:
-            dx, dy = self.x - other.x, self.y - other.y
-            distance = sqrt(dx ** 2 + dy ** 2)
-            if distance < self.radius + other.radius \
-                    and self.radius > other.radius:
-                other.alive = False
-               	self.radius = self.radius + int(other.radius * 0.146)
-
-    def draw(self, screen):
-        """在窗口上绘制球"""
-        pygame.draw.circle(screen, self.color,
-                           (self.x, self.y), self.radius, 0)
+# import csv
+#
+# filename = 'teacher.csv'
+# teachers = [['aaa','18','a--a'],['bbb','19','b--b'],['ccc','20','c--c']]
+#
+# try:
+#     with open(filename,'w') as f:
+#         writer = csv.writer(f)
+#         for teacher in teachers:
+#             writer.writerow([teacher[0],teacher[1],teacher[2]])
+# except BaseException as e:
+#     print('无法写入',filename)
+# else:
+#     print('保存数据')
 
 
-def main():
-    # 定义用来装所有球的容器
-    balls = []
-    # 初始化导入的pygame中的模块
-    pygame.init()
-    # 初始化用于显示的窗口并设置窗口尺寸
-    screen = pygame.display.set_mode((800, 600))
-    print(screen.get_width())
-    print(screen.get_height())
-    # 设置当前窗口的标题
-    pygame.display.set_caption('大球吃小球')
-    # 定义变量来表示小球在屏幕上的位置
-    x, y = 50, 50
-    running = True
-    # 开启一个事件循环处理发生的事件
-    while running:
-        # 从消息队列中获取事件并对事件进行处理
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                x, y = event.pos
-                radius = randint(10, 100)
-                sx, sy = randint(-10, 10), randint(-10, 10)
-                color = Color.random_color()
-                ball = Ball(x, y, radius, sx, sy, color)
-                balls.append(ball)
-        screen.fill((255, 255, 255))
-        for ball in balls:
-            if ball.alive:
-                ball.draw(screen)
-            else:
-                balls.remove(ball)
-        pygame.display.flip()
-        # 每隔50毫秒就改变小球的位置再刷新窗口
-        pygame.time.delay(50)
-        for ball in balls:
-            ball.move(screen)
-            for other in balls:
-                ball.eat(other)
+# 判断文件内容是否存在xxx
+
+# filename = 'aa.txt'
+# bb = '123123'
+# with open(filename) as f:
+#     lines = f.readlines()
+#     aa = ''
+#     for line in lines:
+#         aa+=line.strip()
+#     if bb in aa:
+#         print('存在')
+
+#  try 异常捕获
+input_again = True
+
+while input_again:
+    try:
+        a = int(input('a='))
+        b = int(input('b='))
+        print('%d / %d = %f' % (a, b, a / b))
+    except ValueError:
+        print('数不对')
+    except ZeroDivisionError:
+        print('除数不能为0')
 
 
-if __name__ == '__main__':
-    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
