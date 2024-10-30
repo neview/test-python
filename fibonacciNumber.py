974969bf1e99275d08bbfb11075c13ccbfddbda3
芒果集团 <1282261308@qq.com>
2024-10-30 17:38:40 +0800

    '进程通信'

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -1645,6 +1645,8 @@ from pygame.draw import lines
 # 多种方式读取文件
 import time
 
+from pygame.examples.go_over_there import target_position
+
 # filename = 'aa.txt'
 # def main():
 #     # with open(filename, 'r',encoding='utf-8') as f:
@@ -1839,9 +1841,156 @@ class Aa(object):
 #     unittest.main()
 
 
-f = open('aa.txt', 'r')
-print(f.read())
-f.close()
+# f = open('aa.txt', 'r')
+# print(f.read())
+# f.close()
+
+import os
+# print(os.name)
+# print(os.environ)
+# print(os.environ.get('ALLUSERSPROFILE'))
+
+# print(os.path.abspath('.')) # 当前目录的路径
+# print(os.path.join(r'\python\/test','text-10'))
+# os.mkdir(r'\python\test\text-10')
+
+# print([x for x in os.listdir('.') if os.path.isdir(x)])
+# print([x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1]=='.py'])
+
+import pickle
+# d = dict(name='AA',age=20,score=88)
+# f = open('aa.txt','wb')
+# pickle.dump(d,f)
+# f.close()
+
+# f = open('aa.txt','rb')
+# d = pickle.load(f)
+# f.close()
+# print(d)
+
+
+# import os
+# import multiprocessing as Process
+#
+# def run_proc(name):
+#     print('Run child process %s (%s)...' % (name, os.getpid()))
+#
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Process(target=run_proc,args=('test',))
+#     print('Child process start.')
+#     p.start()
+#     print('Child process end.')
+#     p.join()
+
+# from multiprocessing import Pool
+# import os, time, random
+#
+# def long_time_task(name):
+#     print('Run task %s (%s)...' % (name, os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random() * 3)
+#     end = time.time()
+#     print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+#
+# if __name__=='__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Pool(4)
+#     for i in range(5):
+#         p.apply_async(long_time_task, args=(i,))
+#     print('Waiting for all subprocesses done...')
+#     p.close()
+#     p.join()
+#     print('All subprocesses done.')
+
+# import subprocess
+#
+# print('$ nslookup')
+# p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+# print(output.decode('utf-8'))
+# print('Exit code:', p.returncode)
+
+from multiprocessing import Process, Queue
+import os, time, random
+
+# 写数据进程执行的代码:
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+# 读数据进程执行的代码:
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+if __name__=='__main__':
+    # 父进程创建Queue，并传给各个子进程：
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    # 启动子进程pw，写入:
+    pw.start()
+    # 启动子进程pr，读取:
+    pr.start()
+    # 等待pw结束:
+    pw.join()
+    # pr进程里是死循环，无法等待其结束，只能强行终止:
+    pr.terminate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
